f3c50813a5fb389e64b457a2e59612c47cde367c
Alex Zosyk <alekcandrzosyk@gmail.com>
2019-08-15 23:50:55 +0300

    implemented "done todo item" logic

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,14 +2,34 @@ import React, {Component} from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
+
+
+    state = {
+        done: false
+    };
+
+    onItemClick = () => {
+        this.setState((prevState) => (
+        {
+            done: !prevState.done
+        }));
+    };
+
+
     render() {
         const {value, important = false} = this.props;
         const style = {
             color: important ? 'steelblue' : 'black',
             fontWeight: important ? 'bold' : 'normal'
         };
+
+        let classes = 'todo-list-item';
+        if (this.state.done) {
+           classes += " done";
+        }
+
         return (
-            <span className="todo-list-item">
+            <span className={classes} onClick={this.onItemClick}>
             <span className="todo-list-item-label"
                   style={style}>
                 {value}
